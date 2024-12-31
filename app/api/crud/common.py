d0c44f6cbf8bb2ae9628d7257662d63e6eb90e57
kojamad28 <kojamad28@gmail.com>
2025-01-01 07:04:15 +0900

    Initial commit

---
+++ app/api/crud/common.py
@@ -0,0 +1,44 @@
+from fastapi import HTTPException
+from sqlmodel import SQLModel, Session, select
+
+
+def create_model(session: Session, model: type[SQLModel], created_model: SQLModel):
+    db_model = model.from_orm(created_model)
+    session.add(db_model)
+    session.commit()
+    session.refresh(db_model)
+    return db_model
+
+
+def retrieve_models(session: Session, model: type[SQLModel], offset: int, limit: int):
+    models = session.exec(select(model).offset(offset).limit(limit)).all()
+    return models
+
+
+def retrieve_model(session: Session, model: type[SQLModel], index_col: str, target: str):
+    db_model = session.exec(select(model).where(getattr(model, index_col) == target)).first()
+    if not db_model:
+        raise HTTPException(status_code=404, detail=f'{target} not found')
+    return db_model
+
+
+def update_model(session: Session, model: type[SQLModel], index_col: str, target: str, updated_model: SQLModel):
+    db_model = session.exec(select(model).where(getattr(model, index_col) == target)).first()
+    if not db_model:
+        raise HTTPException(status_code=404, detail=f'{target} not found')
+    updated_model_data = updated_model.dict(exclude_unset=True)
+    for key, value in updated_model_data.items():
+        setattr(db_model, key, value)
+    session.add(db_model)
+    session.commit()
+    session.refresh(db_model)
+    return db_model
+
+
+def delete_model(session: Session, model: type[SQLModel], index_col: str, target: str):
+    db_model = session.exec(select(model).where(getattr(model, index_col) == target)).first()
+    if not db_model:
+        raise HTTPException(status_code=404, detail=f'{target} not found')
+    session.delete(db_model)
+    session.commit()
+    return {'ok': True}
---
+++ app/api/dependencies.py
@@ -0,0 +1,29 @@
+from dotenv import dotenv_values
+from sqlalchemy import URL
+from sqlmodel import SQLModel, Session, create_engine
+
+config = dotenv_values("postgres/.env.dev")
+#config = dotenv_values("postgres/.env")
+
+DEBUG = config.get("DEBUG", "false").lower() == "true"
+
+db_url = URL.create(
+        "postgresql+psycopg",
+        username=config["POSTGRES_USER"],
+        password=config["POSTGRES_PASSWORD"],  # plain (unescaped) text
+        host=config["POSTGRES_HOST"],
+        port=config["POSTGRES_PORT"],
+        database=config["POSTGRES_DB"],
+        query={"options": f"-c search_path={config['POSTGRES_SCHEMA']}"},
+)
+connect_args = {}
+engine = create_engine(db_url, echo=DEBUG, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
